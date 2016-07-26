3fdc17e13a6d4a65ec1c56b4e368d1e3ab3af6d6
Chaman <chaman@Chamans-MacBook-Air.local>
2016-07-26 16:48:47 +0530

    Add initial design and js

---
+++ mypomo.js
@@ -0,0 +1,22 @@
+$( document ).ready (function() {
+  var str = $('.text-set').text();
+  var str1 = str.slice(7);
+  console.log(parseInt(str1));
+
+  $('.fa-chevron-up').click(function() {
+
+    console.log('Hurray');
+
+  });
+  $('.fa-chevron-down').click(function() {
+
+    console.log('Hurray');
+
+  });
+
+  $('button').click(function() {
+
+      });
+
+
+});
